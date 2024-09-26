7148f7d382adab3a91353ff65e70598a38b14b63
Sebastiaan Jansen <sebastiaan.jansen@dpgmedia.be>
2024-09-26 11:55:27 +0200

    windows

--- app/components/map-2d.tsx
+++ app/components/map-2d.tsx
@@ -1,52 +1,49 @@
-
 import type { House } from '~/domain/house'
 import { Wall } from './wall'
 import { useMemo } from 'react'
 
 export function Map2d({ house }: { house: House }) {
-
   const viewBox = useMemo(() => {
+    // offset with 20cm to make sure the walls are visible
+    const offset = 20
     // get the minimum and maximum x and y values of the walls of all floors
-    const minX = Math.min(
-      ...house.floors.flatMap((floor) =>
-        floor.walls.map((wall) => wall.start.x)
-      )
-    )
-    const minY = Math.min(
-      ...house.floors.flatMap((floor) =>
-        floor.walls.map((wall) => wall.start.y)
-      )
-    )
-    const maxX = Math.max(
-      ...house.floors.flatMap((floor) =>
-        floor.walls.map((wall) => wall.start.x)
-      )
-    )
-    const maxY = Math.max(
-      ...house.floors.flatMap((floor) =>
-        floor.walls.map((wall) => wall.start.y)
-      )
-    )
+    const minX =
+      Math.min(
+        ...house.floors.flatMap((floor) => floor.walls.map((wall) => wall.start.x)),
+      ) - offset
+    const minY =
+      Math.min(
+        ...house.floors.flatMap((floor) => floor.walls.map((wall) => wall.start.y)),
+      ) - offset
+    const maxX =
+      Math.max(
+        ...house.floors.flatMap((floor) => floor.walls.map((wall) => wall.start.x)),
+      ) +
+      2 * offset
+    const maxY =
+      Math.max(
+        ...house.floors.flatMap((floor) => floor.walls.map((wall) => wall.start.y)),
+      ) +
+      2 * offset
     return `${minX} ${minY} ${maxX} ${maxY}`
   }, [house])
- 
-  return (
 
-      <svg
-        width="100%"
-        height="100%"
-        role="img"
-        aria-label="Huizer"
-        className="bg-gray-200"
-        viewBox={viewBox}
-      >
-        {house.floors[0].walls.map((wall) => (
-          <Wall
-            key={`${wall.start.x}-${wall.start.y}-${wall.end.x}-${wall.end.y}`}
-            wall={wall}
-          />
-        ))}
-        1
-      </svg>
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      role="img"
+      aria-label="Huizer"
+      className="bg-gray-200"
+      viewBox={viewBox}
+    >
+      {house.floors[0].walls.map((wall) => (
+        <Wall
+          key={`${wall.start.x}-${wall.start.y}-${wall.end.x}-${wall.end.y}`}
+          wall={wall}
+        />
+      ))}
+      1
+    </svg>
   )
 }
