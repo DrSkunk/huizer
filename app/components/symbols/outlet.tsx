c772f167c1c99fad4a203008813a06094aeeb417
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2024-10-19 12:09:19 +0200

    add an amount to outlet

--- app/components/symbols/outlet.tsx
+++ app/components/symbols/outlet.tsx
@@ -1,13 +1,25 @@
-export function Outlet() {
+export function Outlet({ amount }: { amount: number }) {
   return (
-    <g stroke="currentColor" fill="none" transform="translate(-10,-10)">
-      <line x1="0" y1="10" x2="12" y2="10" />
-      <path d="M 20 2 A 1 1 0 0 0 20 18" />
-      {/* Aarding */}
-      <line x1="12" y1="2" x2="12" y2="18" />
-      {/* Kinderbescherming */}
-      <line x1="20" y1="0" x2="20" y2="2" />
-      <line x1="20" y1="18" x2="20" y2="20" />
+    <g>
+      {Array.from({ length: amount }).map((_, index) => (
+        <g
+          key={`outlet-${
+            // biome-ignore lint/suspicious/noArrayIndexKey: There is no unique ID for the amount of outlets
+            index
+          }`}
+          stroke="currentColor"
+          fill="none"
+          transform={`translate(${index * 12},0)`}
+        >
+          <line x1="0" y1="10" x2="12" y2="10" />
+          <path d="M 20 2 A 1 1 0 0 0 20 18" />
+          {/* Aarding */}
+          <line x1="12" y1="2" x2="12" y2="18" />
+          {/* Kinderbescherming */}
+          <line x1="20" y1="0" x2="20" y2="2" />
+          <line x1="20" y1="18" x2="20" y2="20" />
+        </g>
+      ))}
     </g>
   )
 }
