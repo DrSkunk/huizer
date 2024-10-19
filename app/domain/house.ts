3b4365bdcc2147ea80ee55e1c15e8a0c62484c6d
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2024-10-19 20:59:07 +0200

    init Zod

--- app/domain/house.ts
+++ app/domain/house.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export const defaults = {
   door: {
     width: 100,
@@ -20,87 +22,213 @@ export enum Orientation {
   VERTICAL = "vertical",
 }
 
-export type PositionXY = {
-  x: number;
-  y: number;
-};
-
-export type PositionXZ = {
-  x: number;
-  z: number;
-};
-
-// A length is a number with a unit
-// currently unused
-export type Length = {
-  value: number;
-  unit: "m" | "cm" | "mm";
-};
-
-export type House = {
-  floors: Floor[];
-};
-
-export type Floor = {
-  name: string;
-  walls: Wall[];
-};
-
-export type Wall = {
-  start: PositionXY;
-  end: PositionXY;
-  thickness: number;
-  doors: Door[];
-  windows: Window[];
-  panels: Panel[];
-};
-
-export type Door = {
-  position: number; // how far along the wall it is
-  width?: number;
-  height?: number;
-};
-
-export type Window = {
-  position: PositionXZ;
-  width?: number;
-  height?: number;
-};
-
-export type Circuit = {
-  name: string;
-  ID: string;
-};
-
 export enum Side {
   LEFT = "left",
   RIGHT = "right",
 }
-
-export type Panel = {
-  position: PositionXY;
-  orientation: Orientation;
-  side: Side;
-  items: PanelItem[];
-};
 
 export enum PanelItemType {
   OUTLET = "outlet",
   LIGHT_SWITCH = "light-switch",
 }
 
-interface BasePanelItem {
-  ID: string;
-}
+export const PositionXYSchema = z.object({
+  x: z.number(),
+  y: z.number(),
+});
 
-interface Outlet extends BasePanelItem {
-  type: PanelItemType.OUTLET;
-  amount: number;
-}
+export const PositionXZSchema = z.object({
+  x: z.number(),
+  z: z.number(),
+});
 
-interface LightSwitch extends BasePanelItem {
-  type: PanelItemType.LIGHT_SWITCH;
-  circuitIDs: string[];
-}
+export const LengthSchema = z.object({
+  value: z.number(),
+  unit: z.enum(["m", "cm", "mm"]),
+});
 
-export type PanelItem = Outlet | LightSwitch;
+export const DoorSchema = z.object({
+  position: z.number(),
+  width: z.number().optional(),
+  height: z.number().optional(),
+});
+
+export const WindowSchema = z.object({
+  position: PositionXZSchema,
+  width: z.number().optional(),
+  height: z.number().optional(),
+});
+
+export const CircuitSchema = z.object({
+  name: z.string(),
+  ID: z.string(),
+});
+
+const BasePanelItemSchema = z.object({
+  ID: z.string(),
+});
+
+const OutletSchema = BasePanelItemSchema.extend({
+  type: z
+    .nativeEnum(PanelItemType)
+    .refine((val) => val === PanelItemType.OUTLET),
+  amount: z.number(),
+});
+
+const LightSwitchSchema = BasePanelItemSchema.extend({
+  type: z
+    .nativeEnum(PanelItemType)
+    .refine((val) => val === PanelItemType.LIGHT_SWITCH),
+  circuitIDs: z.array(z.string()),
+});
+
+export const PanelItemSchema = z.union([OutletSchema, LightSwitchSchema]);
+
+export const PanelSchema = z.object({
+  position: PositionXYSchema,
+  orientation: z.nativeEnum(Orientation),
+  side: z.nativeEnum(Side),
+  items: z.array(PanelItemSchema),
+});
+
+export const WallSchema = z.object({
+  start: PositionXYSchema,
+  end: PositionXYSchema,
+  thickness: z.number(),
+  doors: z.array(DoorSchema),
+  windows: z.array(WindowSchema),
+  panels: z.array(PanelSchema),
+});
+
+export const FloorSchema = z.object({
+  name: z.string(),
+  walls: z.array(WallSchema),
+});
+
+export const HouseSchema = z.object({
+  floors: z.array(FloorSchema),
+});
+
+export type PositionXY = z.infer<typeof PositionXYSchema>;
+export type PositionXZ = z.infer<typeof PositionXZSchema>;
+export type Length = z.infer<typeof LengthSchema>;
+export type Door = z.infer<typeof DoorSchema>;
+export type Window = z.infer<typeof WindowSchema>;
+export type Circuit = z.infer<typeof CircuitSchema>;
+export type PanelItem = z.infer<typeof PanelItemSchema>;
+export type Panel = z.infer<typeof PanelSchema>;
+export type Wall = z.infer<typeof WallSchema>;
+export type Floor = z.infer<typeof FloorSchema>;
+export type House = z.infer<typeof HouseSchema>;
+
+export const defaultHouse: House = {
+  floors: [
+    {
+      name: "Ground floor",
+      walls: [
+        {
+          start: { x: 0, y: 0 },
+          end: { x: 1000, y: 0 },
+          thickness: 30,
+          doors: [
+            {
+              position: 100,
+              width: 100,
+            },
+          ],
+          windows: [
+            {
+              position: { x: 220, z: 0 },
+              height: 220,
+              width: 120,
+            },
+            {
+              position: { x: 527, z: 0 },
+              height: 220,
+              width: 270,
+            },
+          ],
+          panels: [
+            {
+              position: { x: 400, y: 0 },
+              orientation: Orientation.VERTICAL,
+              side: Side.RIGHT,
+              items: [
+                {
+                  ID: "1",
+                  type: PanelItemType.OUTLET,
+                  amount: 5,
+                },
+                {
+                  ID: "2",
+                  type: PanelItemType.OUTLET,
+                  amount: 2,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          start: { x: 1000, y: 0 },
+          end: { x: 1000, y: 1000 },
+          thickness: 30,
+          doors: [],
+          windows: [],
+          panels: [],
+        },
+        {
+          start: { x: 1000, y: 1000 },
+          end: { x: 0, y: 1000 },
+          thickness: 30,
+          doors: [],
+          windows: [],
+          panels: [],
+        },
+        {
+          start: { x: 0, y: 1000 },
+          end: { x: 0, y: 0 },
+          thickness: 30,
+          doors: [],
+          windows: [],
+          panels: [],
+        },
+        {
+          start: { x: 500, y: 0 },
+          end: { x: 600, y: 1000 },
+          thickness: 9,
+          doors: [
+            {
+              position: 100,
+            },
+          ],
+          windows: [
+            {
+              position: { x: 220, z: 0 },
+              height: 220,
+              width: 120,
+            },
+            {
+              position: { x: 527, z: 0 },
+              height: 220,
+              width: 270,
+            },
+          ],
+          panels: [
+            {
+              position: { x: 500, y: 0 },
+              orientation: Orientation.VERTICAL,
+              side: Side.LEFT,
+              items: [
+                {
+                  ID: "1",
+                  type: PanelItemType.OUTLET,
+                  amount: 5,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
--- app/hooks/useLocalStorage.tsx
+++ app/hooks/useLocalStorage.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { Orientation, PanelItemType, Side, type House } from '~/domain/house'
+import { defaultHouse, HouseSchema, type House } from '~/domain/house'
+import { z } from 'zod'
 
 export function useLocalStorage() {
   const [house, setHouse] = useState<null | House>(null)
@@ -7,120 +8,31 @@ export function useLocalStorage() {
   useEffect(() => {
     const storedHouse = localStorage.getItem('house')
     if (storedHouse) {
-      setHouse(JSON.parse(storedHouse))
-    } else {
-      setHouse({
-        floors: [
-          {
-            name: 'Ground floor',
-            walls: [
-              {
-                start: { x: 0, y: 0 },
-                end: { x: 1000, y: 0 },
-                thickness: 30,
-                doors: [
-                  {
-                    position: 100,
-                    width: 100,
-                  },
-                ],
-                windows: [
-                  {
-                    position: { x: 220, z: 0 },
-                    height: 220,
-                    width: 120,
-                  },
-                  {
-                    position: { x: 527, z: 0 },
-                    height: 220,
-                    width: 270,
-                  },
-                ],
-                panels: [
-                  {
-                    position: { x: 400, y: 0 },
-                    orientation: Orientation.VERTICAL,
-                    side: Side.RIGHT,
-                    items: [
-                      {
-                        ID: '1',
-                        type: PanelItemType.OUTLET,
-                        amount: 5,
-                      },
-                      {
-                        ID: '2',
-                        type: PanelItemType.OUTLET,
-                        amount: 2,
-                      },
-                    ],
-                  },
-                ],
-              },
-              {
-                start: { x: 1000, y: 0 },
-                end: { x: 1000, y: 1000 },
-                thickness: 30,
-                doors: [],
-                windows: [],
-                panels: [],
-              },
-              {
-                start: { x: 1000, y: 1000 },
-                end: { x: 0, y: 1000 },
-                thickness: 30,
-                doors: [],
-                windows: [],
-                panels: [],
-              },
-              {
-                start: { x: 0, y: 1000 },
-                end: { x: 0, y: 0 },
-                thickness: 30,
-                doors: [],
-                windows: [],
-                panels: [],
-              },
-              {
-                start: { x: 500, y: 0 },
-                end: { x: 600, y: 1000 },
-                thickness: 9,
-                doors: [
-                  {
-                    position: 100,
-                  },
-                ],
-                windows: [
-                  {
-                    position: { x: 220, z: 0 },
-                    height: 220,
-                    width: 120,
-                  },
-                  {
-                    position: { x: 527, z: 0 },
-                    height: 220,
-                    width: 270,
-                  },
-                ],
-                panels: [
-                  {
-                    position: { x: 500, y: 0 },
-                    orientation: Orientation.VERTICAL,
-                    side: Side.LEFT,
-                    items: [
-                      {
-                        ID: '1',
-                        type: PanelItemType.OUTLET,
-                        amount: 5,
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      })
+      try {
+        const loadedHouse = JSON.parse(storedHouse)
+        // check if it matches with zod
+        const parsedHouse = z.object({
+          name: z.string(),
+          rooms: z.array(
+            z.object({
+              name: z.string(),
+              width: z.number(),
+              length: z.number(),
+              windows: z.number(),
+              doors: z.number(),
+              color: z.string(),
+            }),
+          ),
+        })
+        const result = HouseSchema.parse(loadedHouse)
+        setHouse(result)
+
+        return
+      } catch (e) {
+        console.error('Error parsing stored house:', e)
+      }
     }
+    setHouse(defaultHouse)
   }, [])
 
   useEffect(() => {
